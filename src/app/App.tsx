3d4c1cd74b04b374f2473455e23dc815b6cf12ec
josedrios <joserios0531@gmail.com>
2025-11-18 17:24:09 -0800

    Created and styled footer component, tweaked nav spacing (margin/padding)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import Nav from '@/components/nav.tsx';
 import Question from '@/components/question.tsx';
 import CommentSection from '@/comments/comment-section';
+import Footer from '@/components/footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Nav />
         <Question />
         <CommentSection />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__body">
+        <a className="footer__link">BACK TO TOP</a>
+        <a className="footer__link">ABOUT</a>
+        <a className="footer__link">DONATE</a>
+        <a className="footer__link">FAQS</a>
+        <a className="footer__link">CONTACT</a>
+        <a className="footer__link">TOS</a>
+      </div>
+      <p className="footer__copyright">Copyright © 2025 Jose D. Rios</p>
+    </div>
+  );
+}
