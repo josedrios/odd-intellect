698dbea3bac0190b021362b62e7a0d2b9cb4d94e
Jose Rios <joserios0531@gmail.com>
2026-01-18 09:15:02 -0800

    Db pool connection was set up, test query works as well

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const { Pool } = require("pg");
 
+// Server Setup
+dotenv.config();
 const app = express();
-const PORT = 4000;
-
+app.use(cors());
 app.use(express.json());
+
+// Database Connection
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Database Query Test
+pool.connect().then(async () => {
+  const res = await pool.query("SELECT * FROM birds;");
+  console.log(res.rows);
+});
+
+// Server listening
+const PORT = 4000;
 app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Server is running on port http://0.0.0.0:${PORT}`),
+  console.log(`Server running on http://0.0.0.0:${PORT}`),
 );
