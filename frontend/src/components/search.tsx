41f4e6324841fe112e7481b2b4775111e77fab71
Jose Rios <joserios0531@gmail.com>
2026-02-19 16:49:28 -0800

    Changed home page search bar UI to include users/posts toggle to switch between search type

--- frontend/src/components/search.tsx
+++ frontend/src/components/search.tsx
@@ -3,19 +3,38 @@ import Icon from '@/components/icons';
 import Select from '@/components/select';
 
 export default function SearchBar() {
-  const options = [
+  const searchOptions = [
+    { value: 'posts', label: 'POSTS' },
+    { value: 'users', label: 'USERS' },
+  ];
+  const sortOptions = [
     { value: 'relevant', label: 'RELEVANT' },
     { value: 'newest', label: 'NEWEST' },
     { value: 'popular', label: 'POPULAR' },
   ];
-
   return (
     <form className="search-bar">
-      <button type="submit" className="btn--bordered">
-        <Icon name={ICON.SEARCH} />
-      </button>
-      <input className="search-bar__input" placeholder={`Find a post`} />
-      <Select options={options} defaultValue={options[0]} size={'xs'} />
+      <div className="search-bar__header">
+        <input className="search-bar__input" placeholder={`Find a post`} />
+        <button type="submit" className="btn--bordered">
+          <Icon name={ICON.SEARCH} />
+        </button>
+      </div>
+      <div className="search-bar__footer">
+        <p>TYPE:</p>
+        <Select
+          options={searchOptions}
+          defaultValue={searchOptions[0]}
+          size={'xs'}
+        />
+        <p>{'     '} </p>
+        <p>SORT:</p>
+        <Select
+          options={sortOptions}
+          defaultValue={sortOptions[0]}
+          size={'xs'}
+        />
+      </div>
     </form>
   );
 }
